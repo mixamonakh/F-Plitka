f216eaf96a4678cdbdd0389e2b2016b285297632
Mikhail Monakhov <mixa.monakh@gmail.com>
2022-07-18 00:21:22 +0300

    сделал слайдер и кнопки хедера

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,19 @@
 new WOW().init();
+
+var slider = tns({
+    container: '#autoHeight',
+    autoHeight: true,
+    items: 1,
+    swipeAngle: false,
+    nav: false,
+    controls: false,
+    speed: 400
+});
+
+document.querySelector('.promo-arrows__prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.promo-arrows__next').addEventListener('click', function () {
+    slider.goTo('next');
+});
